f8f5a3606a4efdfd7d9022dec90d1cc390b18c2d
Aure <aurelm95@gmail.com>
2022-04-28 18:24:20 +0200

    palabras más precisas y avance convolucion full

--- model/convolution.py
+++ model/convolution.py
@@ -47,27 +47,39 @@ class Convolucion:
             result.append(fila)
         return np.array(result)
     
-    def aplicar_convolucion_full(self, imagen):
+    def aplicar_convolucion_full(self, imagen, debug=False):
         """
         Applies convolution to a given image with the current filter, stride and padding
 
         Image has to be in the form channels first
+
+        Notes:
+        
+            - We are using numpy slicing: image[a:b,c:d,e:f]
+            - np.sum sums all the elemets os a tensor of any dimension
+
+
         
         
         """
         result=[]
-        # asumo que imagen es una np.array cuadrada de lado .shape[0]
-        tamaño_output=int(np.floor((imagen.shape[0]+2*self.padding-self.tamaño_filtro)/self.stride)+1) # en realidad deberia servir unicamente como debug para comprobar las dimensiones del output
+        channels, height, width = imagen.shape
+        if debug: 
+            assert height==width, "ERROR: The image is not squared!"
+        tamaño_output=int(np.floor((height+2*self.padding-self.tamaño_filtro)/self.stride)+1) # en realidad deberia servir unicamente como debug para comprobar las dimensiones del output
         print("tamaño_output:",tamaño_output)
         imagen=np.pad(imagen,self.padding)
         # print(imagen)
-        for i in range(tamaño_output):
-            fila=[]
-            for j in range(tamaño_output):
-                region=imagen[i:i+self.tamaño_filtro,j:j+self.tamaño_filtro]
-                fila.append(np.sum(region*self.filtro))
-            result.append(fila)
+        for k in range(channels):
+            for i in range(tamaño_output):
+                fila=[]
+                for j in range(tamaño_output):
+                    region=imagen[:,i*self.stride:i*self.stride+self.tamaño_filtro,j*self.stride:j*self.stride+self.tamaño_filtro]
+                    fila.append(np.sum(region*self.filtro))
+                result.append(fila)
         return np.array(result)
+    
+    
 
         
 
@@ -88,9 +100,51 @@ igual=np.array([
     [0,0,0]
 ])
 
-if __name__=='__main__':
+
+
+t1=np.array([
+    [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ],
+    [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ],
+    [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+])
+
+t2=np.array([
+    [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ],
+    [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ],
+    [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ]
+])
+
+
+if __name__=='__main__' and 0:
     c=Convolucion(filtro=igual)
     t0=time.time()
     cuno=c.aplicar_convolucion_ingenua(uno)
     print("timepo:",round(time.time()-t0,4))
     print(cuno)
+
+if __name__=='__main__':
+    t=t1*t2
